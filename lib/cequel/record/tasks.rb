02b9a60126b75dc92b7f5e5ca5a38af29ab78416
Oren Mazor <oren.mazor@gmail.com>
2016-01-11 12:57:41 -0500

    the current migration tactic depends on re-loading all of the app/model files and detecting any new constants within. refactor to something simpler which is guaranteed to work and not have any namespace issues

--- lib/cequel/record/tasks.rb
+++ lib/cequel/record/tasks.rb
@@ -1,5 +1,8 @@
 # -*- encoding : utf-8 -*-
-task :environment do
+
+unless defined?(Rails)
+ task :environment do
+  end
 end
 
 namespace :cequel do
@@ -46,42 +49,11 @@ namespace :cequel do
   end
 
   def migrate
-    watch_stack = ActiveSupport::Dependencies::WatchStack.new
-
-    migration_table_names = Set[]
-    project_root = defined?(Rails) ? Rails.root : Dir.pwd
-    models_dir_path = "#{File.expand_path('app/models', project_root)}/"
-    model_files = Dir.glob(File.join(models_dir_path, '**', '*.rb'))
-    model_files.sort.each do |file|
-      watch_namespaces = ["Object"]
-      model_file_name = file.sub(/^#{Regexp.escape(models_dir_path)}/, "")
-      dirname = File.dirname(model_file_name)
-      watch_namespaces << dirname.classify unless dirname == "."
-      watch_stack.watch_namespaces(watch_namespaces)
-      require_dependency(file)
-
-      new_constants = watch_stack.new_constants
-      if new_constants.empty?
-        new_constants << model_file_name.sub(/\.rb$/, "").classify
-      end
-
-      new_constants.each do |class_name|
-        # rubocop:disable HandleExceptions
-        begin
-          clazz = class_name.constantize
-        rescue LoadError, NameError, RuntimeError
-        else
-          if clazz.is_a?(Class)
-            if clazz.ancestors.include?(Cequel::Record) &&
-                !migration_table_names.include?(clazz.table_name.to_sym)
-              clazz.synchronize_schema
-              migration_table_names << clazz.table_name.to_sym
-              puts "Synchronized schema for #{class_name}"
-            end
-          end
-        end
-        # rubocop:enable HandleExceptions
-      end
+    models = Dir['app/models/**/*.rb'].reject{|f| f.start_with? "app/models/concern"}.map {|f| File.basename(f, '.*').camelize.constantize }
+    cequel_models = models.select {|m| m.ancestors.include? Cequel::Record }.uniq 
+    cequel_models.each do |model|
+      model.synchronize_schema
+      puts "Synchronized schema for #{model}"
     end
   end
 end
